194b0ef0ad6386f0374b5e2acf801fe1c7a5f948
iamkiran27 <saikiran.kavali@zemosolabs.com>
2021-12-31 08:45:36 +0530

    modified

--- src/CLeanCode/ApplicationInterface.java
+++ src/CLeanCode/ApplicationInterface.java
@@ -3,10 +3,7 @@ package CLeanCode;
 public interface ApplicationInterface {
     void showProducts();
     PaymentInterface selectPaymetOption();
-<<<<<<< HEAD
     void showOrderStatus();
     void trackOrder();
 
-=======
->>>>>>> f56b7cf9dd253dcd91d7be808c27d143a6efbf96
 }
